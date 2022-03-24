df714363c84c486e64d8c3460983db0567f145a4
郑伟鑫 <3482525266@qq.com>
2022-03-24 16:44:50 +0800

    增加了echarts

--- src/main.js
+++ src/main.js
@@ -1,26 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+// 导入echarts
+import * as echarts from 'echarts';
 // 导入element ui组件样式
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css';
 // 导入按需加载的element ui组件
-import element from '@/element/index.js'
+import element from '@/element/index.js';
 // 初始化全局样式
-Vue.use(element)
+Vue.use(element);
 // 加载动画库
-import animated from 'animate.css'
-Vue.use(animated)
+import animated from 'animate.css';
+Vue.use(animated);
 // 图片懒加载
-import VueLazyload from 'vue-lazyload'
-Vue.use(VueLazyload)
+import VueLazyload from 'vue-lazyload';
+Vue.use(VueLazyload);
 // flexible移动端适配
-import 'amfe-flexible'
+import 'amfe-flexible';
 
-Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts;
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app')
+}).$mount('#app');
