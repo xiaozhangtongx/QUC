db697d604b821ea89b626371680fc6cc67f03ba9
郑伟鑫 <3482525266@qq.com>
2022-04-05 21:15:58 +0800

    增加了HomePage页面,但是还没替换图片和内容

---
+++ src/views/HomePage/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="shell">
+    <div class="image" style="background-image: url('./images/1.jpg')"></div>
+    <div class="heading">
+      <h1>When you are confused</h1>
+    </div>
+    <div class="text">
+      <h1>Set goals in your mind</h1>
+    </div>
+
+    <div class="image" style="background-image: url('./images/2.jpg')"></div>
+    <div class="heading">
+      <h1>When you're down</h1>
+    </div>
+    <div class="text">
+      <h1>Try to wake up the beast in your heart</h1>
+    </div>
+
+    <div class="image" style="background-image: url('./images/3.jpg')"></div>
+    <div class="heading">
+      <h1>When prople leave you</h1>
+    </div>
+    <div class="text">
+      <h1>It's time to start your season</h1>
+    </div>
+
+    <div class="image" style="background-image: url('./4.jpg')"></div>
+    <div class="heading">
+      <h1>Come on,stranger.</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.shell {
+  height: 100vh;
+  overflow-x: hidden;
+  perspective: 3px;
+}
+
+.shell div {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-style: 30px;
+  letter-spacing: 2px;
+}
+
+.image {
+  transform: translateZ(-1px) scale(1.6);
+  background-size: cover;
+  height: 100vh;
+  z-index: -1;
+}
+
+.text {
+  height: 50vh;
+  background-color: #fff;
+}
+
+.text h1 {
+  color: #000;
+}
+
+.heading {
+  z-index: -1;
+  transform: translateY(-30vh) translateZ(1px);
+  color: #fff;
+  font-size: 30px;
+}
+</style>
