fa1fcb27c5c820b48326887e60e683f3f99ab6db
郑伟鑫 <3482525266@qq.com>
2022-04-23 23:30:44 +0800

    地图还没完成

--- src/views/Echarts/index.vue
+++ src/views/Echarts/index.vue
@@ -1,57 +1,26 @@
 <template>
   <div id="body">
-    <div class="left">
-      <Map />
-    </div>
-    <div class="right">
-      <div id="ltop">
-        <Percent />
-      </div>
-      <div id="lbottom">
-        <WordCloud />
-      </div>
-    </div>
+    <Map />
   </div>
 </template>
 
 <script>
-  import Percent from './components/Percent'
-  import WordCloud from './components/WordCloud'
-  import Map from './components/Map'
+import Map from './components/Map';
 
-  export default {
-    name: '',
-    data() {
-      return {}
-    },
-    methods: {},
-    components: {
-      Percent,
-      WordCloud,
-      Map,
-    },
-  }
+export default {
+  name: '',
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    Map,
+  },
+};
 </script>
 
 <style scoped lang="less">
-  #body {
-    display: flex;
-  }
-  .left {
-    flex: 1.7;
-  }
-  .right {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .lbottom,
-  .ltop {
-    flex: 1;
-  }
-  .lbottom {
-    width: 100%;
-    height: 100%;
-  }
+#body {
+  display: flex;
+}
 </style>
